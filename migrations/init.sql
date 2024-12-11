73f51b97692c28e8db309f059f4d9036247dddb5
oceanzove <zove888@gmail.com>
2024-12-11 13:42:30 +0500

    Добавляет скрипт создание таблицы регламентов

--- migrations/init.sql
+++ migrations/init.sql
@@ -9,6 +9,14 @@ CREATE TABLE "Account" (
                            "role" VARCHAR NOT NULL REFERENCES "Role"("name")
 );
 
+-- Таблица с регламентами
+CREATE TABLE "Regulation" (
+                              "id" SERIAL PRIMARY KEY, -- Уникальный идентификатор регламента
+                              "title" VARCHAR NOT NULL, -- Название регламента
+                              "content" TEXT, -- Содержимое регламента
+                              "account_email" VARCHAR NOT NULL REFERENCES "Account"("email") ON DELETE CASCADE -- Привязка к аккаунту
+);
+
 -- Вставляем данные в таблицу Role
 INSERT INTO "Role" ("name") VALUES
                                 ('Admin'),
@@ -17,10 +25,16 @@ INSERT INTO "Role" ("name") VALUES
 
 -- Вставляем данные в таблицу Account
 INSERT INTO "Account" ("email", "password", "role") VALUES
-                                ('zove', '$2a$10$QkjvoLbAM3bDlCgDqu/G4eMfdu0FcLAPSXj4OjwKBRXC79jiJaMtO', 'Admin');
+                                ('test', '$2a$10$A6lwFl3TlDyZA3UkbobGFOFe.PyI0iQsEUlm1rILs7HWGAaV3AKcC', 'Admin');
 
+-- Вставляем данные в таблицу Regulation
+INSERT INTO "Regulation" ("title", "content", "account_email") VALUES
+                                                                   ('Регламент 1', 'Содержимое регламента 1', 'test'),
+                                                                   ('Регламент 2', 'Содержимое регламента 2', 'test');
 
 -- Индексы для повышения производительности
+CREATE INDEX idx_account_email ON "Regulation" ("account_email");
+
 -- CREATE INDEX idx_process_id ON regulations (process_id);
 -- CREATE INDEX idx_regulation_id ON sections (regulation_id);
 -- CREATE INDEX idx_regulation_view_regulation_id ON regulation_views (regulation_id);
